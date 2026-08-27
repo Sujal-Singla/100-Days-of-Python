b1e8aaad5d9009220e08d12eb95169c74260ccb0
Sujal <biharibanke095@gmail.com>
2026-08-27 17:57:09 +0000

    First commit

---
+++ Day1/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the brand Name Generator")
+city = input("What city does you grown in up \n")
+name = input("What is the name of your Fristpet \n")
+print("Your brand name is ", city, name)
